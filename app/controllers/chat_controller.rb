ca4783c748a8f8ddcc9b6df9b4dba4608a42f1db
owner <owner@owner-no-MacBook-Air1.local>
2018-12-25 01:35:34 +0900

    とりあえず投稿が男の場合のみではあるがそれっぽく動いた

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,4 +1,20 @@
 class ChatController < ApplicationController
   def index
+    @chats = Chat.order(_id: :asc)
+  end
+
+  def create
+    # フォームから送信されたデータを受け取り、保存する
+    @chat = Chat.new(sex: params[:selectedSex],
+       userName: params[:inputUserName],
+       comments: params[:inputComment]
+    )
+    #rootページにredirect
+    if @chat.save
+      redirect_to("/chat/index")
+    else
+      @errorMessage = "更新に失敗"
+      render("/chat/index")    
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'chat/index'
+  # get 'chat/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chat#index'
+
+  post "/chat/index" => "chat#index"
+  get "/chat/index" => "chat#index"
+  post "/chat/create" => "chat#create"
+  
 end
