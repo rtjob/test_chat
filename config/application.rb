ee165da9f3c54e7d2ce95b27a60ed48f116e938d
owner <owner@owner-no-MacBook-Air1.local>
2018-12-24 17:41:25 +0900

    Precompiling assets failed.がまたでたため。記載場所変更

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module TestChat
   class Application < Rails::Application
