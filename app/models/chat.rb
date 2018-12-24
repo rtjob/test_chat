6338f816b24f1923fd634658ecb12d07103cddfc
owner <owner@owner-no-MacBook-Air1.local>
2018-12-25 00:00:33 +0900

    カラムとvalidate追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,16 @@
 class Chat
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :id, type: String
+  field :userName, type: String
+  field :contents, type: String
+
+  # _idカラムに関するバリデーション
+  validates :id,{presence: true}
+  # userNameカラムに関するバリデーション
+  validates :userName,{presence: true,length: { maximum: 20 }}
+  # contentsカラムに関するバリデーション
+  validates :contents,{presence: true,length: { maximum: 140 }}
+
 end
