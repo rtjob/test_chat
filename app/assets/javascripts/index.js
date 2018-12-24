30023540932d35fc3d42667b29d2edac302fca91
owner <owner@owner-no-MacBook-Air1.local>
2018-12-24 23:29:23 +0900

    選択外のとき何も表示しないように変更

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,16 +6,26 @@ function changeSexForced() {
 
     if (selectedMan.checked) {
         //選択地変更することでCSSを変更する
+        selectedLabelMan.classList.remove("btn-default");
+        selectedLabelMan.classList.add("btn-success");
         selectedLabelMan.classList.add("focus");
         selectedLabelMan.classList.add("actice");
+
+        selectedLabelWoman.classList.remove("btn-danger");
+        selectedLabelWoman.classList.add("btn-default");
         selectedLabelWoman.classList.remove("focus");
         selectedLabelWoman.classList.remove("actice");
 
         selectedWoman.checked = false;
     } else {
         //選択地変更することでCSSを変更する
+        selectedLabelMan.classList.remove("btn-success");
+        selectedLabelMan.classList.add("btn-default");
         selectedLabelMan.classList.remove("focus");
         selectedLabelMan.classList.remove("actice");
+
+        selectedLabelWoman.classList.remove("btn-default");
+        selectedLabelWoman.classList.add("btn-danger");
         selectedLabelWoman.classList.add("focus");
         selectedLabelWoman.classList.add("actice");
 
