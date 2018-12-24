05c6b1b5ba449f513367d32f74d0b9feb68ecaef
owner <owner@owner-no-MacBook-Air1.local>
2018-12-24 18:03:03 +0900

    さらにエラーの修正

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,5 @@ module TestChat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.initialize_on_precompile = false
   end
 end
-
-config.assets.initialize_on_precompile = false
