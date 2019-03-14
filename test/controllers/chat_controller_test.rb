aff938be005d3fc9e58db977626a38b564dfd9a4
owner <owner@owner-no-MacBook-Air1.local>
2019-03-15 00:44:39 +0900

    Revert "少しだけテスト書いた"
    This reverts commit e0e1ff73534a78ea46764128e732d208d27a3b2b.

--- test/controllers/chat_controller_test.rb
+++ test/controllers/chat_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class ChatControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get root_path
-    assert_template '/chat/index'
+    get chat_index_url
     assert_response :success
   end
 
